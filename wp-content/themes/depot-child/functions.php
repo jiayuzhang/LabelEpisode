1d14fa425534fd118d7c1143aed735c905cb3de0
rozzerhmq <hmq_2006@hotmail.com>
2019-04-20 21:46:08 -0400

    reformat using PhpStorm formatter adjusting to Google code style

--- wp-content/themes/depot-child/functions.php
+++ wp-content/themes/depot-child/functions.php
@@ -1,65 +1,75 @@
 <?php
 
 /*** Child Theme Function  ***/
-if ( ! function_exists( 'depot_mikado_child_theme_enqueue_scripts' ) ) {
+if (!function_exists('depot_mikado_child_theme_enqueue_scripts')) {
   function depot_mikado_child_theme_enqueue_scripts() {
-    wp_enqueue_style( 'depot-mikado-child-style', get_stylesheet_directory_uri() . '/style.css' );
-    wp_enqueue_script( 'depot-mikado-child-script', get_stylesheet_directory_uri() . '/main.js', array ( 'woo-variation-gallery' ), false, true );
+    wp_enqueue_style('depot-mikado-child-style',
+        get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_script('depot-mikado-child-script',
+        get_stylesheet_directory_uri() . '/main.js',
+        array('woo-variation-gallery'), false, true);
   }
 
   // The parent depot enqueues many stylesheets, to ensure we load the latest, assign a higher
   // priority (the lower the earlier).
-  add_action( 'wp_enqueue_scripts', 'depot_mikado_child_theme_enqueue_scripts', 99 );
+  add_action('wp_enqueue_scripts', 'depot_mikado_child_theme_enqueue_scripts', 99);
 }
 
 //Remove the additional information tab from the product page
-function wp_le_woocommerce_product_tabs( $tabs ) {
-    unset( $tabs['additional_information'] );
-    return $tabs;
+function wp_le_woocommerce_product_tabs($tabs) {
+  unset($tabs['additional_information']);
+  return $tabs;
 }
-add_filter( 'woocommerce_product_tabs', 'wp_le_woocommerce_product_tabs', 99 );
+
+add_filter('woocommerce_product_tabs', 'wp_le_woocommerce_product_tabs', 99);
 
 //====================================================
 // Integration woo-variation-gallery and wc-frontend-manager
 //====================================================
 
 // Add additional gallery when loading manage-product page
-function wp_le_wcfm_variation_edit_data( $variations, $variation_id, $variation_id_key ) {
+function wp_le_wcfm_variation_edit_data($variations, $variation_id, $variation_id_key) {
   // NOTE: $variations->get_image_id returns product feature image if variation image misses
-  $variation_gallery_images = get_post_meta( $variation_id, 'woo_variation_gallery_images', true );
+  $variation_gallery_images =
+      get_post_meta($variation_id, 'woo_variation_gallery_images', true);
   if ($variation_gallery_images) {
     foreach ($variation_gallery_images as $key => $variation_gallery_img_id) {
-      $variation_gallery_images[$key] = array( 'image' => wp_get_attachment_url($variation_gallery_img_id) );
+      $variation_gallery_images[$key] =
+          array('image' => wp_get_attachment_url($variation_gallery_img_id));
     }
     $variations[$variation_id_key]['gallery_images'] = $variation_gallery_images;
   }
   return $variations;
 }
-add_filter( 'wcfm_variation_edit_data', 'wp_le_wcfm_variation_edit_data', 99, 3 );
+
+add_filter('wcfm_variation_edit_data', 'wp_le_wcfm_variation_edit_data', 99, 3);
 
 // Add additional gallery into the variations form on manage-product page
-function wp_le_wcfm_product_manage_fields_variations( $options, $variations ) {
+function wp_le_wcfm_product_manage_fields_variations($options, $variations) {
   // Variation additional gallery (exclude variation featured image)
   // To preserve the display order of options in a form, add a new option
   // `gallery_images` right after `image` (The variation featured image)
   $idx = array_search('image', array_keys($options));
   $offset = $idx + 1;
   return array_slice($options, 0, $offset, true) + array('gallery_images' => array(
-    'type' => 'multiinput',
-    'class' => 'le-variation-gallery wcfm-text wcfm_ele variable variable-subscription wcfm_additional_variation_images',
-    'options' => array(
-      'image' => array(
-        'type' => 'upload',
-        'class' => 'le-variation-gallery-item wcfm_gallery_upload',
-        'prwidth' => 75
-      )
-    )
-  )) + array_slice($options, $offset, NULL, true);
+          'type' => 'multiinput',
+          'class' => 'le-variation-gallery wcfm-text wcfm_ele variable variable-subscription wcfm_additional_variation_images',
+          'options' => array(
+              'image' => array(
+                  'type' => 'upload',
+                  'class' => 'le-variation-gallery-item wcfm_gallery_upload',
+                  'prwidth' => 75
+              )
+          )
+      )) + array_slice($options, $offset, null, true);
 }
-add_filter( 'wcfm_product_manage_fields_variations', 'wp_le_wcfm_product_manage_fields_variations', 99, 3 );
+
+add_filter('wcfm_product_manage_fields_variations',
+    'wp_le_wcfm_product_manage_fields_variations', 99, 3);
 
 // Store additional gallery into post metadata (used by woo-variation-gallery)
-function wp_le_after_wcfm_product_variation_meta_save( $new_product_id, $variation_id, $variations ) {
+function wp_le_after_wcfm_product_variation_meta_save($new_product_id, $variation_id,
+    $variations) {
   global $WCFM;
   if (isset($variations['gallery_images'])) {
     // gallery_images is an array of array(image=>img_url)
@@ -67,19 +77,23 @@ function wp_le_after_wcfm_product_variation_meta_save( $new_product_id, $variati
     $variation_gallery_image_ids = [];
     foreach ($variations['gallery_images'] as $variation_img) {
       if (!empty($variation_img['image'])) {
-        $variation_gallery_image_ids[] = $WCFM->wcfm_get_attachment_id($variation_img['image']);
+        $variation_gallery_image_ids[] =
+            $WCFM->wcfm_get_attachment_id($variation_img['image']);
       }
     }
     if (!empty($variation_gallery_image_ids)) {
-      update_post_meta( $variation_id, 'woo_variation_gallery_images', $variation_gallery_image_ids );
+      update_post_meta($variation_id, 'woo_variation_gallery_images',
+          $variation_gallery_image_ids);
     } else {
-      delete_post_meta( $variation_id, 'woo_variation_gallery_images' );
+      delete_post_meta($variation_id, 'woo_variation_gallery_images');
     }
   } else {
-    delete_post_meta( $variation_id, 'woo_variation_gallery_images' );
+    delete_post_meta($variation_id, 'woo_variation_gallery_images');
   }
 }
-add_action( 'after_wcfm_product_variation_meta_save', 'wp_le_after_wcfm_product_variation_meta_save', 99, 3 );
+
+add_action('after_wcfm_product_variation_meta_save',
+    'wp_le_after_wcfm_product_variation_meta_save', 99, 3);
 
 //====================================================
 // End of Integration woo-variation-gallery and wc-frontend-manager
@@ -93,37 +107,43 @@ add_action( 'after_wcfm_product_variation_meta_save', 'wp_le_after_wcfm_product_
 // Inspired by plugin 'force-default-variant-for-woocommerce'
 
 // Remove 'Choose an option' from dropdown
-function wp_le_woocommerce_dropdown_variation_attribute_options_html( $html, $args ) {
-  if ( empty( $args['selected'] ) ) {
+function wp_le_woocommerce_dropdown_variation_attribute_options_html($html, $args) {
+  if (empty($args['selected'])) {
     return $html;
   }
 
-  $show_option_none_text = $args['show_option_none'] ? $args['show_option_none'] : __( 'Choose an option', 'woocommerce' );
-  $show_option_none_html = '<option value="">' . esc_html( $show_option_none_text ) . '</option>';
+  $show_option_none_text = $args['show_option_none']
+      ? $args['show_option_none']
+      :
+      __('Choose an option', 'woocommerce');
+  $show_option_none_html =
+      '<option value="">' . esc_html($show_option_none_text) . '</option>';
 
   $html = str_replace($show_option_none_html, '', $html);
 
   return $html;
 }
-add_filter( 'woocommerce_dropdown_variation_attribute_options_html', 'wp_le_woocommerce_dropdown_variation_attribute_options_html', 99, 2 );
+
+add_filter('woocommerce_dropdown_variation_attribute_options_html',
+    'wp_le_woocommerce_dropdown_variation_attribute_options_html', 99, 2);
 
 // Returns the default variation option. If not set, choose the first variation id
-function wp_le_woocommerce_product_get_default_attributes( $defaults ) {
+function wp_le_woocommerce_product_get_default_attributes($defaults) {
   global $product;
 
-  if ( !$product ) {
+  if (!$product) {
     return $defaults;
   }
 
-  if ( !empty( $defaults ) ) {
+  if (!empty($defaults)) {
     return $defaults;
   }
 
-  if ( $product->post_type !== 'product' ) {
+  if ($product->post_type !== 'product') {
     return $defaults;
   }
 
-  if ( !$product->is_type( 'variable' ) ) {
+  if (!$product->is_type('variable')) {
     return $defaults;
   }
 
@@ -132,33 +152,38 @@ function wp_le_woocommerce_product_get_default_attributes( $defaults ) {
   sort($children);
 
   // Gets the first child product variation (if any)
-  $variation = wc_get_product( $children[0] );
-  if ( empty( $variation ) ) {
+  $variation = wc_get_product($children[0]);
+  if (empty($variation)) {
     return $defaults;
   }
 
   $attr = $variation->get_attributes();
   $defaults = array();
-  foreach( $attr as $key => $value ) {
+  foreach ($attr as $key => $value) {
     $defaults[$key] = $value;
   }
   return $defaults;
 }
-add_filter( 'woocommerce_product_get_default_attributes', 'wp_le_woocommerce_product_get_default_attributes', 99, 1 );
+
+add_filter('woocommerce_product_get_default_attributes',
+    'wp_le_woocommerce_product_get_default_attributes', 99, 1);
 
 // Remove the Clear selection link.
-function wp_le_woocommerce_reset_variations_link( $value ) {
+function wp_le_woocommerce_reset_variations_link($value) {
   return '';
 }
-add_filter( 'woocommerce_reset_variations_link', 'wp_le_woocommerce_reset_variations_link', 99, 1 );
+
+add_filter('woocommerce_reset_variations_link', 'wp_le_woocommerce_reset_variations_link',
+    99, 1);
 //====================================================
 // Remove default unselect variation option
 //====================================================
 
-function wp_le_console_log_var( $var ) {
-    wp_le_console_log(print_r($var, true));
+function wp_le_console_log_var($var) {
+  wp_le_console_log(print_r($var, true));
 }
 
-function wp_le_console_log( $message ) {
-    echo "<script>console.log(`{$message}`)</script>";
+function wp_le_console_log($message) {
+  echo "<script>console.log(`{$message}`)</script>";
 }
+
