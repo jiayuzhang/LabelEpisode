c40346198b5be42c34b21e449148a976f255b0c7
jiayu <shalunhasiti1989@gmail.com>
2019-05-12 18:07:07 -0400

    fix breadcrumb in product category (product-list) page

--- wp-content/themes/depot/framework/modules/title/title-functions.php
+++ wp-content/themes/depot/framework/modules/title/title-functions.php
@@ -263,11 +263,15 @@ if(!function_exists('depot_mikado_custom_breadcrumbs')) {
 
             $output .= '<div class="mkd-breadcrumbs"><div class="mkd-breadcrumbs-inner" itemprop="breadcrumb"><a itemprop="url" '.depot_mikado_get_inline_style($bread_style).' href="' . $homeLink . '">' . $home . '</a>' . $delimiter;
 
-            if ( is_category() || (depot_mikado_is_woocommerce_installed() && depot_mikado_is_product_category())) {
+            if ( is_category()) {
                 $thisCat = get_category(get_query_var('cat'), false);
                 if (isset($thisCat->parent) && $thisCat->parent != 0) $output .= get_category_parents($thisCat->parent, TRUE, ' ' . $delimiter);
                 $output .= $before . single_cat_title('', false) . $after;
-
+            } elseif (depot_mikado_is_woocommerce_installed() && depot_mikado_is_product_category()) {
+              $term = get_term_by('slug', get_query_var('product_cat'), 'product_cat');
+              $output .= get_term_parents_list(
+                $term->term_id, 'product_cat', array('separator' => $delimiter, 'inclusive' => false));
+              $output .= $before . single_cat_title('', false) . $after;
             } elseif (is_tax('portfolio-category')) {
                 $portfolio_category = wp_get_post_terms(get_the_ID(), 'portfolio-category');
                 $portfolio_category = $portfolio_category[0];
