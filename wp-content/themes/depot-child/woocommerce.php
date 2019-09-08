dc510d4331209b0749fe6cdc521b5c24c61ec501
rozzer <hmq_2006@hotmail.com>
2019-09-07 20:06:18 -0400

    format

--- wp-content/themes/depot-child/woocommerce.php
+++ wp-content/themes/depot-child/woocommerce.php
@@ -4,7 +4,7 @@ Template Name: WooCommerce
 */
 ?>
 <?php
-$mkd_sidebar_layout  = depot_mikado_sidebar_layout();
+$mkd_sidebar_layout = depot_mikado_sidebar_layout();
 
 get_header();
 depot_mikado_get_title();
@@ -12,31 +12,30 @@ get_template_part('slider');
 
 //Woocommerce content
 //category_description(); description
-if ( ! is_singular('product') ) { ?>
-	<div class="mkd-container">
-		<div class="mkd-container-inner clearfix">
-			<div class="mkd-grid-row">
-
-				<div <?php echo depot_mikado_get_content_sidebar_class(); ?>>
-          <?php if(!is_tax( 'product_brand' )) { ?>
-  					<div><?php echo category_description(); ?></div>
+if (!is_singular('product')) { ?>
+  <div class="mkd-container">
+    <div class="mkd-container-inner clearfix">
+      <div class="mkd-grid-row">
+        <div <?php echo depot_mikado_get_content_sidebar_class(); ?>>
+          <?php if (!is_tax('product_brand')) { ?>
+            <div><?php echo category_description(); ?></div>
           <?php } ?>
-					<?php depot_mikado_woocommerce_content(); ?>
-				</div>
-				<?php if($mkd_sidebar_layout !== 'no-sidebar') { ?>
-					<div <?php echo depot_mikado_get_sidebar_holder_class(); ?>>
-						<?php get_sidebar(); ?>
-					</div>
-				<?php } ?>
-			</div>
-		</div>
-	</div>
+          <?php depot_mikado_woocommerce_content(); ?>
+        </div>
+        <?php if ($mkd_sidebar_layout !== 'no-sidebar') { ?>
+          <div <?php echo depot_mikado_get_sidebar_holder_class(); ?>>
+            <?php get_sidebar(); ?>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
 <?php } else { ?>
-	<div class="mkd-container">
-		<div class="mkd-container-inner clearfix">
-			<div><?php echo category_description(); ?></div>
-			<?php depot_mikado_woocommerce_content(); ?>
-		</div>
-	</div>
+  <div class="mkd-container">
+    <div class="mkd-container-inner clearfix">
+      <div><?php echo category_description(); ?></div>
+      <?php depot_mikado_woocommerce_content(); ?>
+    </div>
+  </div>
 <?php } ?>
 <?php get_footer(); ?>
